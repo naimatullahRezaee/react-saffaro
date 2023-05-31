4ba02c104dfeffaad18f22e8c30f349f8bfaecd1
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-05-31 13:41:30 +0200

    work on different urls

--- src/componnets/TripsList.js
+++ src/componnets/TripsList.js
@@ -3,17 +3,17 @@ import "./TripList.css";
 
 export default function TripsList() {
   const [trips, setTrips] = useState([]);
-
+  const [url, setUrl] = useState("http://localhost:3000/trips");
   // fetch("http://localhost:3000/trips")
   //   .then((response) => response.json())
   //   .then((json) => setTrip(json));
 
   // console.log(trips);
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    fetch(url)
       .then((response) => response.json())
       .then((json) => setTrips(json));
-  }, []);
+  }, [url]);
   return (
     <div className="tirp-list">
       <h2>Trips List</h2>
@@ -21,10 +21,28 @@ export default function TripsList() {
         {trips.map((trip) => (
           <li key={trip.id}>
             <h3>{trip.title}</h3>
+            <p>{trip.location}</p>
             <p>{trip.price}</p>
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() =>
+            setUrl("http://localhost:3000/trips?location=Afghanistan")
+          }
+        >
+          Aghani Trips
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?location=Iran")}
+        >
+          Iranian Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 }
