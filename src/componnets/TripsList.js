66ab90ae8c98445a5b7bb6c676b14ee7aeac04df
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-05-31 14:31:17 +0200

    use custom hook

--- src/componnets/TripsList.js
+++ src/componnets/TripsList.js
@@ -1,30 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./TripList.css";
+import { useFetch } from "../hooks/UseFetch";
 
 export default function TripsList() {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
   // fetch("http://localhost:3000/trips")
   //   .then((response) => response.json())
   //   .then((json) => setTrip(json));
 
   // console.log(trips);
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setTrips(json));
-  }, [url]);
+  const trips = useFetch(url);
+
   return (
     <div className="tirp-list">
       <h2>Trips List</h2>
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.location}</p>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.location}</p>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
       <div className="filters">
         <button
